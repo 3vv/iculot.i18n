e9a0fc5847ac0c68690584c7cb5aba42e929357a
h <h@>
2018-12-17 12:02:58 +0800

    Init iculot.i18n

---
+++ language/language.go
@@ -0,0 +1,175 @@
+// h 20181217
+//
+// Language Code
+
+package language
+
+var (
+	// L may refer to ISO_639
+	//
+	// Codes arranged alphabetically
+	L = map[string]string{
+		// a
+		"aa": "Afar",
+		"ab": "Abkhazian",
+		"af": "Afrikaans",
+		"am": "Amharic",
+		"ar": "Arabic",
+		"as": "Assamese",
+		"ay": "Aymara",
+		"az": "Azerbaijani",
+		// b
+		"ba": "Bashkir",
+		"be": "Byelorussian",
+		"bg": "Bulgarian",
+		"bh": "Bihari",
+		"bi": "Bislama",
+		"bn": "Bengali; Bangla",
+		"bo": "Tibetan",
+		"br": "Breton",
+		// c
+		"ca": "Catalan",
+		"co": "Corsican",
+		"cs": "Czech",
+		"cy": "Welsh",
+		// d
+		"da": "Danish",
+		"de": "German",
+		"dz": "Bhutani",
+		// e
+		"el": "Greek",
+		"en": "English",
+		"eo": "Esperanto",
+		"es": "Spanish",
+		"et": "Estonian",
+		"eu": "Basque",
+		// f
+		"fa": "Persian",
+		"fi": "Finnish",
+		"fj": "Fiji",
+		"fo": "Faeroese",
+		"fr": "French",
+		"fy": "Frisian",
+		// g
+		"ga": "Irish",
+		"gd": "Scots Gaelic",
+		"gl": "Galician",
+		"gn": "Guarani",
+		"gu": "Gujarati",
+		// h
+		"ha": "Hausa",
+		"hi": "Hindi",
+		"hr": "Croatian",
+		"hu": "Hungarian",
+		"hy": "Armenian",
+		// i
+		"ia": "Interlingua",
+		"ie": "Interlingue",
+		"ik": "Inupiak",
+		"in": "Indonesian",
+		"is": "Icelandic",
+		"it": "Italian",
+		"iw": "Hebrew",
+		// j
+		"ja": "Japanese",
+		"ji": "Yiddish",
+		"jw": "Javanese",
+		// k
+		"ka": "Georgian",
+		"kk": "Kazakh",
+		"kl": "Greenlandic",
+		"km": "Cambodian",
+		"kn": "Kannada",
+		"ko": "Korean",
+		"ks": "Kashmiri",
+		"ku": "Kurdish",
+		"ky": "Kirghiz",
+		// l
+		"la": "Latin",
+		"ln": "Lingala",
+		"lo": "Laothian",
+		"lt": "Lithuanian",
+		"lv": "Latvian, Lettish",
+		// m
+		"mg": "Malagasy",
+		"mi": "Maori",
+		"mk": "Macedonian",
+		"ml": "Malayalam",
+		"mn": "Mongolian",
+		"mo": "Moldavian",
+		"mr": "Marathi",
+		"ms": "Malay",
+		"mt": "Maltese",
+		"my": "Burmese",
+		// n
+		"na": "Nauru",
+		"ne": "Nepali",
+		"nl": "Dutch",
+		"no": "Norwegian",
+		// o
+		"oc": "Occitan",
+		"om": "(Afan)Oromo",
+		"or": "Oriya",
+		// p
+		"pa": "Punjabi",
+		"pl": "Polish",
+		"ps": "Pashto, Pushto",
+		"pt": "Portuguese",
+		// q
+		"qu": "Quechua",
+		// r
+		"rm": "Rhaeto - Romance",
+		"rn": "Kirundi",
+		"ro": "Romanian",
+		"ru": "Russian",
+		"rw": "Kinyarwanda",
+		// s
+		"sa": "Sanskrit",
+		"sd": "Sindhi",
+		"sg": "Sangro",
+		"sh": "Serbo - Croatian",
+		"si": "Singhalese",
+		"sk": "Slovak",
+		"sl": "Slovenian",
+		"sm": "Samoan",
+		"sn": "Shona",
+		"so": "Somali",
+		"sq": "Albanian",
+		"sr": "Serbian",
+		"ss": "Siswati",
+		"st": "Sesotho",
+		"su": "Sundanese",
+		"sv": "Swedish",
+		"sw": "Swahili",
+		// t
+		"ta": "Tamil",
+		"te": "Tegulu",
+		"tg": "Tajik",
+		"th": "Thai",
+		"ti": "Tigrinya",
+		"tk": "Turkmen",
+		"tl": "Tagalog",
+		"tn": "Setswana",
+		"to": "Tonga",
+		"tr": "Turkish",
+		"ts": "Tsonga",
+		"tt": "Tatar",
+		"tw": "Twi",
+		// u
+		"uk": "Ukrainian",
+		"ur": "Urdu",
+		"uz": "Uzbek",
+		// v
+		"vi": "Vietnamese",
+		"vo": "Volapuk",
+		// w
+		"wo": "Wolof",
+		// x
+		"xh": "Xhosa",
+		// y
+		"yo": "Yoruba",
+		// z
+		"zh": "Chinese",
+		"zu": "Zulu",
+	}
+)
